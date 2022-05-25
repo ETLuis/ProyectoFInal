abc2b3b146c42b48c60b0ef5d23d19c54ef4170e
Luis Campano Goldar <lcampanogoldar@danielcastelao.org>
2022-05-25 11:53:23 +0200

    ProyectoFinal 0.2

--- src/main/java/InterfazGrafica2.java
+++ src/main/java/InterfazGrafica2.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import javax.swing.JOptionPane;
 
 public class InterfazGrafica2 extends javax.swing.JFrame {
 
@@ -24,12 +25,22 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setText("Introduce una letra");
         jLabel1.setMaximumSize(new java.awt.Dimension(200, 17));
         jLabel1.setMinimumSize(new java.awt.Dimension(200, 17));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 120, -1, 38));
 
         jTextField1.setMargin(new java.awt.Insets(0, 47, 0, 0));
         jTextField1.addActionListener(new java.awt.event.ActionListener() {
@@ -42,6 +53,7 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
                 jTextField1KeyTyped(evt);
             }
         });
+        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(397, 250, 104, 70));
 
         jButton1.setBackground(new java.awt.Color(255, 174, 0));
         jButton1.setText("Comprobar");
@@ -50,8 +62,10 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 256, 157, 60));
 
         jTextField2.setMargin(new java.awt.Insets(0, 95, 0, 0));
+        jPanel1.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 390, 263, 101));
 
         jButton2.setBackground(new java.awt.Color(25, 233, 228));
         jButton2.setText("Escribir Palabra");
@@ -60,6 +74,7 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
                 jButton2ActionPerformed(evt);
             }
         });
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 480, 147, 39));
 
         jTextField3.setEnabled(false);
         jTextField3.addActionListener(new java.awt.event.ActionListener() {
@@ -67,6 +82,7 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
                 jTextField3ActionPerformed(evt);
             }
         });
+        jPanel1.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 530, 124, 50));
 
         jButton3.setBackground(new java.awt.Color(13, 113, 254));
         jButton3.setText("Generar Palabra");
@@ -75,6 +91,7 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
                 jButton3ActionPerformed(evt);
             }
         });
+        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 202, 157, 42));
 
         jButton4.setBackground(new java.awt.Color(254, 1, 181));
         jButton4.setText("Menú Principal");
@@ -83,98 +100,63 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
                 jButton4ActionPerformed(evt);
             }
         });
+        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 530, 154, 70));
 
+        jButton5.setBackground(new java.awt.Color(251, 255, 0));
         jButton5.setText("Comprobar");
         jButton5.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton5ActionPerformed(evt);
             }
         });
+        jPanel1.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 590, -1, -1));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addGap(0, 0, Short.MAX_VALUE)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(480, 480, 480))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                .addGroup(jPanel1Layout.createSequentialGroup()
-                                                        .addGap(359, 359, 359)
-                                                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                                .addGroup(jPanel1Layout.createSequentialGroup()
-                                                        .addGap(295, 295, 295)
-                                                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                        .addGap(35, 35, 35)
-                                                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                                .addContainerGap(204, Short.MAX_VALUE))
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addGap(12, 12, 12)
-                                                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addContainerGap())
-                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addGap(68, 68, 68))))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(32, 32, 32)
-                                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jButton5)
-                                .addGap(59, 59, 59))
-        );
-        jPanel1Layout.setVerticalGroup(
-                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(202, 202, 202)
-                                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 161, Short.MAX_VALUE)
-                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(jButton5)
-                                .addGap(61, 61, 61))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(128, 128, 128)
-                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(26, 26, 26)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(46, 46, 46)
-                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(32, 32, 32))
-        );
+        jLabel2.setText("Fallos:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(875, 42, -1, -1));
+
+        jLabel3.setText("0");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(937, 42, -1, -1));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/0Fallos.png"))); // NOI18N
+        jLabel4.setMaximumSize(new java.awt.Dimension(81, 78));
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 260, -1, -1));
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/1Fallo.png"))); // NOI18N
+        jLabel5.setVisible(false);
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(255, 260, 90, -1));
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/2Fallos.png"))); // NOI18N
+        jLabel6.setVisible(false);
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 260, -1, -1));
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/3Fallos.png"))); // NOI18N
+        jLabel7.setVisible(false);
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 260, -1, -1));
+
+        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/4Fallos.png"))); // NOI18N
+        jLabel8.setVisible(false);
+        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 260, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
-                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 1, Short.MAX_VALUE))
+                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 1012, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
-                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 16, Short.MAX_VALUE))
+                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 711, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>
 
     List<Character> jugadores = new ArrayList<>();
-    String palabraJuego;
+    String palabraJuego="";
 
 
 
@@ -209,7 +191,14 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
     }
 
 
+    public static void repeticionPalabra(String palabraJuego, List<Character> jugadores){
 
+        List<String> palabras = Arrays.asList("camion", "toro", "perro", "hawaii");
+        Random rand = new Random();
+        palabraJuego = palabras.get(rand.nextInt(palabras.size()));
+        System.out.println(palabraJuego);
+
+    }
 
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {
@@ -245,7 +234,7 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
         List<String> palabras = Arrays.asList("camion", "toro", "perro", "hawaii");
         Random rand = new Random();
         palabraJuego = palabras.get(rand.nextInt(palabras.size()));
-        System.out.println(""+palabraJuego);
+        System.out.println(palabraJuego);
 
     }
 
@@ -255,6 +244,7 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
         obx.setVisible(false);
 
 
+
     }
 
     private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {
@@ -262,22 +252,68 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
 
     }
 
+
+    int intentos=0;
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {
         // TODO add your handling code here:
         String palabraComprob=jTextField3.getText();
-        if(palabraComprob=palabraJuego){
-            System.out.println("Palabra correcta");
+
+
+        if(palabraComprob.equals(palabraJuego)){
+            JOptionPane.showMessageDialog(null,"Palabra correcta");
         }
         else{
-            System.out.println("Palabra incorrecta");
+            JOptionPane.showMessageDialog(null,"Palabra incorrecta");
+            intentos++;
+            String i=String.valueOf(intentos);
+
+            jLabel3.setText(i);
+
+            if(intentos==1){
+                jLabel4.setVisible(false);
+                jLabel5.setVisible(true);
+            }else{
+                jLabel5.setVisible(false);
+            }
+
+            if(intentos==2){
+                jLabel6.setVisible(true);
+            }else{
+                jLabel6.setVisible(false);
+            }
+
+            if(intentos==3){
+                jLabel7.setVisible(true);
+            }else{
+                jLabel7.setVisible(false);
+            }
+
+            if(intentos==4){
+                jLabel8.setVisible(true);
+            }else{
+                jLabel8.setVisible(false);
+
+            }
+
+            if(intentos==5){
+                JOptionPane.showMessageDialog(null, "Has perdido :(");
+                jLabel3.setText(""+0);
+
+                InterfazGrafica2.repeticionPalabra(palabraJuego, jugadores);
+
+            }
+
+
         }
 
     }
+
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new InterfazGrafica2().setVisible(true);
+
 
             }
         });
@@ -294,6 +330,13 @@ public class InterfazGrafica2 extends javax.swing.JFrame {
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private static javax.swing.JLabel jLabel5;
+    private static javax.swing.JLabel jLabel6;
+    private static javax.swing.JLabel jLabel7;
+    private static javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     public static javax.swing.JTextField jTextField1;
     private static javax.swing.JTextField jTextField2;
