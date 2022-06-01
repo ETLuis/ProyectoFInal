223528887a7dbace72bff53579fbf7b867b000c5
Luis Campano Goldar <lcampanogoldar@danielcastelao.org>
2022-06-01 14:33:16 +0200

    ProyectoFinal 0.97 (Rama)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
-
+        //Esta es la rama prueba
 
         InterfazGrafica obx=new InterfazGrafica();
         obx.setVisible(true);
